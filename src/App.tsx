141daeb2236b9774d81debea4e587c2f4a8e7464
David Kalina <david@koptional.com>
2022-05-22 21:02:29 -0600

    survey route

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
+import SurveyPage from './pages/SurveyPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
+      <Route path="/survey/:id" element={<SurveyPage />} />
     </Routes>
   )
 }
