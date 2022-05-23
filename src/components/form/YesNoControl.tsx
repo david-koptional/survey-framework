d0d6b33a7b58e0cb51f9fa17671d152c51e0b18f
David Kalina <david@koptional.com>
2022-05-22 21:59:03 -0600

    feat: added yes no control

---
+++ src/components/form/YesNoControl.tsx
@@ -0,0 +1,20 @@
+import { Radio, Text } from '@geist-ui/core'
+import { useUpdateQuestions } from '../../hooks/useUpdateQuestions'
+import { Question } from '../../types'
+
+export const YesNoControl = ({ question }: { question: Question }) => {
+  const [value, setValue] = useUpdateQuestions(question)
+
+  return (
+    <Radio.Group value={value} onChange={val => setValue(val)} useRow>
+      <Radio value="yes" name={'Yes'}>
+        <Text font="16px">Yes</Text>
+      </Radio>
+      <Radio value="no" name={'No'}>
+        <Text font="16px">No</Text>
+      </Radio>
+    </Radio.Group>
+  )
+}
+
+export default YesNoControl
