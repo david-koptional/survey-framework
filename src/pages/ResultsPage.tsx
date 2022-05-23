21f8e4f0831357f556eea97bcf0b2ec910224894
David Kalina <david@koptional.com>
2022-05-22 22:58:12 -0600

    feat: output results

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,10 +1,21 @@
+import { Button } from '@geist-ui/core'
+import { writeFile, BaseDirectory } from '@tauri-apps/api/fs'
+import { downloadDir } from '@tauri-apps/api/path'
 import { useAtom } from 'jotai'
 import { allQuestionsAtom } from '../store/atoms/questions'
 
 const ResultsPage = () => {
   const [allQuestions] = useAtom(allQuestionsAtom)
 
-  return <>{JSON.stringify(allQuestions)}</>
+  const exportResults = async () => {
+    const downloadPath = await downloadDir()
+
+    const path = `${downloadPath}/results.json`
+
+    await writeFile({ path, contents: JSON.stringify(allQuestions) })
+  }
+
+  return <Button onClick={() => exportResults()}>Get Results</Button>
 }
 
 export default ResultsPage
