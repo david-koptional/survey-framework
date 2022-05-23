0665f5afd013224e99e35d461b63c0d37eb6374e
David Kalina <david@koptional.com>
2022-05-22 21:57:45 -0600

    feat: added question lists component

---
+++ src/components/display/QuestionsList.tsx
@@ -0,0 +1,25 @@
+import { Grid } from '@geist-ui/core'
+import GridContainer from '@geist-ui/core/esm/grid/grid-container'
+import { useAtom } from 'jotai'
+import { sectionQuestionsAtom } from '../../store/atoms/questions'
+import FormControl from '../form/FormControl'
+import NextSection from './NextSection'
+
+const QuestionsList = () => {
+  const [sectionQuestions] = useAtom(sectionQuestionsAtom)
+
+  return (
+    <GridContainer justify="center" direction="column">
+      <Grid xs={24} lg={12} justify="center" direction="column">
+        {sectionQuestions.map(q => (
+          <FormControl key={q.id} question={q} />
+        ))}
+      </Grid>
+      <Grid xs>
+        <NextSection />
+      </Grid>
+    </GridContainer>
+  )
+}
+
+export default QuestionsList
